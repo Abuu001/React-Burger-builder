2878bf04e5740e2c4d6208ea0008203f53037e7e
Abuu001 <abulugz@gmail.com>
2020-09-23 10:20:02 +0300

    Added price of the items

--- src/containers/Burger Builder/BurgerBuilder.js
+++ src/containers/Burger Builder/BurgerBuilder.js
@@ -3,6 +3,12 @@ import Aux from "../../hoc/Auxi"
 import Burger from "../../components/Burger/Burger"
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICE={
+    salad: 0.5,
+    bacon : 0.8,
+    cheese : 0.99 ,
+    meat : 0.21 
+}
  class BurgerBuilder extends Component {
     constructor(props) {
         super()
@@ -12,8 +18,27 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
                 bacon : 0,
                 cheese : 0 ,
                 meat : 0 
-            }
+            },
+            totalPrice: 4
         }
+        this.addIngredientHandler=this.addIngredientHandler.bind(this)
+        this.removeIngredientHandler=this.removeIngredientHandler.bind(this)
+    }
+
+    addIngredientHandler=(type)=>{
+        const oldCount = this.state.ingredients
+        const updatedCount= oldCount + 1
+        const updatedIngredients ={
+            ...this.state.ingredients
+        } 
+
+        updatedIngredients[type] = updatedCount
+        const priceAddition= INGREDIENT_PRICE[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+    }
+    removeIngredientHandler=(type)=>{
+
     }
     render() {
         return (
