0851d3487e51f64a934b865e673d7abfd16cf8cf
Abuu001 <abulugz@gmail.com>
2020-09-24 23:07:41 +0300

    Added a navigation bar

---
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./ToolBar.css"
+
+
+const ToolBar=props=>(
+    <header className="ToolBar">
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>....</nav>
+    </header>
+)
+
+export default ToolBar;
