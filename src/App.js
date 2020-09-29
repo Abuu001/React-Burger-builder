29e95bfbdac59a2730859233c4da0070cad981c6
Abuu001 <abulugz@gmail.com>
2020-09-29 12:15:42 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import  Layout from "./components/Layout/Layout"
-import BurgerBuilder from "./containers/Burger Builder/BurgerBuilder"
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/Burger Builder/BurgerBuilder";
 
 function App(props) {
   return (
--- src/components/Burger/OrderSummery/OrderSummary.js
+++ src/components/Burger/OrderSummery/OrderSummary.js
@@ -1,29 +1,36 @@
-import React from "react"
+import React,{Component} from "react"
 import Aux from "../../../hoc/Auxi"
 import Button from "../../UI/Button/Button"
 
-const OrderSummary=(props)=>{
-    const ingredientSummary =  Object.keys(props.ingredients)
-        .map(igKey=>{
-        return (
-        <li key={igKey}>
-            <span style={{textTransform:"capitalize"}} >{igKey}</span> : {props.ingredients[igKey]}
-        </li>)      
-        })
+class OrderSummary extends Component{
+
+    componentDidUpdate(){
+        console.log("[Ordersummary] will update")
+    }
+    render(){
+        const ingredientSummary =  Object.keys(this.props.ingredients)
+            .map(igKey=>{
+            return (
+            <li key={igKey}>
+                <span style={{textTransform:"capitalize"}} >{igKey}</span> : {this.props.ingredients[igKey]}
+            </li>)      
+            })
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious  burger with the following ingredients : </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price : ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout ? </p>
+                <Button clicked={this.props.purchaseContinued}>Continue</Button>
+                <Button  clicked={this.props.purchaseCancelled}>Cancel</Button>
+            </Aux>
+        )
+
+    }
                                                                                                                                  
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious  burger with the following ingredients : </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price : ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout ? </p>
-            <Button clicked={props.purchaseContinued}>Continue</Button>
-            <Button  clicked={props.purchaseCancelled}>Cancel</Button>
-        </Aux>
-    )
 }
 
 export default OrderSummary
